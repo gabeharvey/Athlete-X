17753014b428b4a1e92e1c49cb7d0ce30fdcf656
Gabe Harvey <harvey7@gmail.com>
2024-09-22 23:50:23 -0500

    Added Navbar.jsx and Home.jsx components

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import '../App.css'; 
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Athlete X</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import '../App.css'; 
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Athlete X</div>
+      <ul className="nav-links">
+        <li className="nav-item">
+          <a href="#" className="nav-link">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">About</a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
