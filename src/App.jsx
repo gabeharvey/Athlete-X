896fbc5322ab2bbe45125180b4cdcf40d818a409
Gabe Harvey <harvey7@gmail.com>
2024-09-23 09:12:51 -0500

    Added background images to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from '../src/components/Navbar.jsx';
 import Footer from '../src/components/Footer.jsx';
 import Home from '../src/components/Home.jsx';
+import About from '../src/components/About.jsx'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
       <Footer />
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import '../App.css'; 
+
+function About() {
+  return (
+    <div className="about-container">
+    </div>
+  );
+}
+
+export default About;
