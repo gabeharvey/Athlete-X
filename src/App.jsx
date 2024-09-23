686dad9428c296ac15c44e038de852d2c2354c27
Gabe Harvey <harvey7@gmail.com>
2024-09-23 12:27:47 -0500

    Added Packages.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from '../src/components/Navbar.jsx';
 import Footer from '../src/components/Footer.jsx';
 import Home from '../src/components/Home.jsx';
-import About from '../src/components/About.jsx'
+import About from '../src/components/About.jsx';
+import Packages from '../src/components/Packages.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Packages />
       <Footer />
     </div>
   );
---
+++ src/components/Packages.jsx
@@ -0,0 +1,10 @@
+import '../App.css'; 
+
+function Packages() {
+  return (
+    <div className="packages-container">
+    </div>
+  );
+}
+
+export default Packages;
