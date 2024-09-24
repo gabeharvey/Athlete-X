4cb9f107be2c0f2cd1d7ecda0b4cf89efc31c9f8
Gabe Harvey <harvey7@gmail.com>
2024-09-24 18:32:29 -0500

    Added Log In link to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
           <a href="#" className="nav-link">About</a>
         </li>
         <li className="nav-item">
-          <a href="#" className="nav-link">Contact</a>
+          <a href="#" className="nav-link">Log In</a>
         </li>
       </ul>
     </nav>
