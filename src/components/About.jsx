ebbfcdea2a173d8a86fd6f6ca79db347a891a3f2
Gabe Harvey <harvey7@gmail.com>
2024-10-01 13:21:49 -0500

    Added google font and h1 to About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,12 @@
-import '../App.css'; 
+import '../App.css';
 
 function About() {
   return (
     <div className="about-container">
+      <h1>
+        Welcome to <span className="tilt-prism">Athlete X</span>! You have done all the hard work and sacrifice. Allow us to present your Elite Athlete to the world!
+      </h1>
+      <h2>Mobile friendly, yet responsive app showcasing athletic abilities.</h2>
     </div>
   );
 }
