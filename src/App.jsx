85b4ba5c3e167fdc134bfede0329f8f9c15173f0
Gabe Harvey <harvey7@gmail.com>
2024-10-01 10:38:30 -0500

    Major styling changes added throughout and carousel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'; 
 import Navbar from '../src/components/Navbar.jsx';
 import Footer from '../src/components/Footer.jsx';
 import Home from '../src/components/Home.jsx';
 import About from '../src/components/About.jsx';
 import Packages from '../src/components/Packages.jsx';
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'black', 
+        color: '#FFFDD0', 
+      },
+    },
+  },
+});
+
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <Packages />
-      <Footer />
-    </div>
+    <ChakraProvider theme={theme}> 
+      <div>
+        <Navbar />
+        <Home />
+        <About />
+        <Packages />
+        <Footer />
+      </div>
+    </ChakraProvider>
   );
 }
 
