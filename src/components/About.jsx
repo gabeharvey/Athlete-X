99db5413a57b0a00d4bf62fec16136ea0c13d519
Gabe Harvey <harvey7@gmail.com>
2024-10-01 14:33:40 -0500

    Added sound effects to About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,36 @@
+import { useState, useRef } from 'react';
 import '../App.css';
 
 function About() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isH2Visible, setIsH2Visible] = useState(false);
+  const [isButtonVisible, setIsButtonVisible] = useState(true); 
+  const audioRef = useRef(null); 
+  const handleStart = () => {
+    setIsVisible(true); 
+    audioRef.current.play(); 
+    setTimeout(() => {
+      setIsH2Visible(true);
+    }, 3000);
+
+    setIsButtonVisible(false); 
+  };
+
   return (
     <div className="about-container">
-      <h1>
+      {isButtonVisible && ( 
+        <button className="clickMe" onClick={handleStart} style={{ marginTop: '2px' }}>
+          Click & Learn More!
+        </button>
+      )}
+      <h1 className={isVisible ? 'zoom-in' : ''}>
         Welcome to <span className="tilt-prism">Athlete X</span>! You have done all the hard work and sacrifice. Allow us to present your Elite Athlete to the world!
       </h1>
-      <h2>Mobile friendly, yet responsive app showcasing athletic abilities.</h2>
+      <div className="fancy-divider"></div>
+      <h2 className={isH2Visible ? 'zoom-in' : ''}>
+        Mobile friendly, yet responsive app showcasing athletic abilities.
+      </h2>
+      <audio ref={audioRef} src="/outdoor-basketball-bounce.mp3" preload="auto" />
     </div>
   );
 }
