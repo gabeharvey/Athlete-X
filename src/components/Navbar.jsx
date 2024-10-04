6680b846b18e919313e82ce704fb71a2ccd82554
Gabe Harvey <harvey7@gmail.com>
2024-10-04 10:52:11 -0500

    Minor update to font size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
         <Heading
           as={RouterLink}
           to="/"
-          fontSize="4xl"
+          fontSize="2xl"
           fontFamily="'Tilt Prism', sans-serif"
           color="#FFFDD0"
           letterSpacing="wider"
