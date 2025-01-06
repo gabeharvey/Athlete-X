bbd05596ef45ab96c392cb65774624c76eea4920
Gabe Harvey <harvey7@gmail.com>
2025-01-06 09:15:22 -0600

    Updated Zoom Animations in About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,11 +39,9 @@ function About() {
       </h2>
       <div className="fancy-divider"></div>
       {isH3Visible && (
-        <div className="zoom-in">
-          <h3>
-            It is crucial to stand out! The talent and skills are there. People want to see it quickly and First Class!
-          </h3>
-        </div>
+        <h3 className="zoom-in">
+          It is crucial to stand out! The talent and skills are there. People want to see it quickly and First Class!
+        </h3>
       )}
       <p className="centered-text">
         Do not see your sport listed? <span className="tilt-prism">Athlete X Elite</span> has you covered! 
@@ -59,16 +57,36 @@ function About() {
       <div className="social-media-section">
         <p className="social-media-text">Follow <span className="tilt-prism">Athlete X Elite</span> on Social Media</p>
         <div className="social-icons">
-          <a href="https://www.facebook.com/people/Athlete-X-Elite/61568944931104/?mibextid=LQQJ4d&rdid=TEUVJDRMaCDf59oU&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F17sXncDzHY%2F%3Fmibextid%3DLQQJ4d" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+          <a
+            href="https://www.facebook.com/people/Athlete-X-Elite/61568944931104/?mibextid=LQQJ4d&rdid=TEUVJDRMaCDf59oU&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F17sXncDzHY%2F%3Fmibextid%3DLQQJ4d"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Facebook"
+          >
             <FaFacebook size={30} style={{ margin: '0 10px', color: '#3b5998' }} />
           </a>
-          <a href="https://www.instagram.com/athlete_x_elite" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+          <a
+            href="https://www.instagram.com/athlete_x_elite"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
             <FaInstagram size={30} style={{ margin: '0 10px', color: '#E4405F' }} />
           </a>
-          <a href="https://www.threads.net/athlete_x_elite" target="_blank" rel="noopener noreferrer" aria-label="Threads">
+          <a
+            href="https://www.threads.net/athlete_x_elite"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Threads"
+          >
             <PiThreadsLogoBold size={30} style={{ margin: '0 10px', color: '#FFFDD0' }} />
           </a>
-          <a href="https://www.tiktok.com/@athlete_x_elite" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
+          <a
+            href="https://www.tiktok.com/@athlete_x_elite"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="TikTok"
+          >
             <FaTiktok size={30} style={{ margin: '0 10px', color: '#69C9D0' }} />
           </a>
         </div>
