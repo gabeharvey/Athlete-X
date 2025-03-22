49f4a69a6a7b5cba9ec768a7b13d1a29d63bf7e7
Gabe Harvey <harvey7@gmail.com>
2025-03-22 17:26:43 -0500

    Update Pricing on Packages.jsx

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -140,7 +140,7 @@ function Packages() {
           }}
         >
           <Text fontSize="2xl" fontWeight="bold" color="#FFFDD0" fontFamily="'Changa', cursive">
-            Media Package $799.00
+            Media Package $699.00
           </Text>
           <Text mt={4} color="#FFFDD0" fontFamily="'Changa', cursive">
             Media Package includes everything in Showcase Package. Plus, Athlete X Elite will professionally capture athlete photography and videography. We come to the event. This content will be included in the custom athlete website.
